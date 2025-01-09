45bc6f14a0cd6f90c5ed82916e2569d0f468b56a
Madiha Babri <mybabri2@gmail.com>
2025-01-09 02:09:34 +0000

    admin.py added for bookings

---
+++ bookings/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import Booking
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    """Class to view the Bookings in the admin panel"""
+
+    list_display = (
+        "owner",
+        "email",
+        "appointment_date",
+        "design_style",
+        "status"
+    )
+    list_filter = ("appointment_date", "owner")
+    search_fields = ["owner", "design_style"]
+    actions = ["confirm_booking"]
+
+    def confirm_booking(self, request, queryset):
+        queryset.update(confirmed=True)
