ff6d06f2d8cabc35c5a3b4aabd93671e2e22c886
Madiha Babri <mybabri2@gmail.com>
2025-01-10 15:22:28 +0000

    booking model changed, new app created named consultancy, model created and migrations done

---
+++ consultancy/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+DESIGNS = (("bedrooms", "Bedroom"), ("washroom", "Washroom"), ("kitchen", "Kitchen"), ("livigroom", "LivingRoom"))
+
+# Create your models here.
+class consultancy(models.Model):
+
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="username_booking"
+    )
+    date_of_booking = models.DateTimeField()
+    design_type = models.TextField(choices=DESIGNS)
+    confirmed = models.BooleanField(default=False)
+    message = models.TextField(max_length=500)
+
+    class Meta:
+        ordering = ["date_of_booking"]
+
+    def __str__(self):
+        return (
+            f"Booking for {self.username} on {self.date_of_booking}"
+        )
--- lets_renovate/settings.py
+++ lets_renovate/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
     'cloudinary',
     'blog',
     'about',
-    'bookings',
+    'consultancy',
     'gallery',
 ]
 
